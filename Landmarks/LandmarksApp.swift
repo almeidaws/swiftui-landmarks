5f3a1e39ea18c0b26648a3d38eb5236717450f20
Gustavo Amaral <almeidaws@users.noreply.github.com>
2023-11-03 14:00:47 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Gustavo Amaral on 03/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
